3d640a544002c3779ff50e5ddd7235b11abfa9c3
ho9 <69878252+ho9@users.noreply.github.com>
2024-03-16 15:04:21 +0100

    Update viewangles.sh

--- source-code/mobile/viewangles.sh
+++ source-code/mobile/viewangles.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-mkdir shared
+
+if [ ! -d "shared" ]; then
+        mkdir shared
+fi
+
 mount -t ios . shared
 clear
+
 while true; do
         line=$(tail -n 1 shared/console.log | cut -c 35- | rev | cut -c 13- | rev)
         printf "\r%s" "viewangles: $line"
